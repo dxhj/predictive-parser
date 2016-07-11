be1b86e5f27f4e3fc5031749dfa41d43c4f942a3
Victor Costa <dxhj@hotmail.com.br>
2016-07-11 20:26:14 -0300

    Add files via upload

---
+++ examples/nullable.py
@@ -0,0 +1,28 @@
+from predictive import PredictiveParser
+
+parser = PredictiveParser("E", {
+	"E": [
+		["T", "E'"]
+	],
+	"E'": [
+		["+", "T", "E'"], [""] 
+	],
+	"T": [
+		["F", "T'"]
+	],
+	"T'": [
+		["*", "F", "T'"], [""]
+	],
+	"F": [
+		["(", "E", ")"], ["id"]
+	]
+
+})
+
+for symbol, nullable in parser.null_dict.iteritems():
+	if symbol in parser.terminals:
+		print symbol + ": " + str(nullable)
+
+for symbol, nullable in parser.null_dict.iteritems():
+	if symbol in parser.nonterminals:
+		print symbol + ": " + str(nullable)
