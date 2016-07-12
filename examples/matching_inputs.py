cbee0051fec2574ea3c4e3f7c9b6a281b5ab2f7d
Victor Costa <dxhj@hotmail.com.br>
2016-07-12 17:49:30 -0300

    Update matching_inputs.py

--- examples/matching_inputs.py
+++ examples/matching_inputs.py
@@ -16,45 +16,45 @@ else:
 parser.verbose_match(["id", "+", "id"])
 """
  .verbose_match() outputs the sequence of derivation steps:
-	** Action: reduce E on `id` to: T E'
-	** Action: reduce T on `id` to: F T'
-	** Action: reduce F on `id` to: id
+	** Action: derive E on `id` to: T E'
+	** Action: derive T on `id` to: F T'
+	** Action: derive F on `id` to: id
 	** Action: match `id`
-	** Action: reduce T' on `+` to: ε
-	** Action: reduce E' on `+` to: + T E'
+	** Action: derive T' on `+` to: ε
+	** Action: derive E' on `+` to: + T E'
 	** Action: match `+`
-	** Action: reduce T on `id` to: F T'
-	** Action: reduce F on `id` to: id
+	** Action: derive T on `id` to: F T'
+	** Action: derive F on `id` to: id
 	** Action: match `id`
-	** Action: reduce T' on `$` to: ε
-	** Action: reduce E' on `$` to: ε
+	** Action: derive T' on `$` to: ε
+	** Action: derive E' on `$` to: ε
 """
 
 parser.verbose_match(["id", "+", "id"], True)
 """
  If the second param is True, it displays the stack at each step:
 	Stack: ['$', 'E']
-	** Action: reduce E on `id` to: T E'
+	** Action: derive E on `id` to: T E'
 	Stack: ['$', "E'", 'T']
-	** Action: reduce T on `id` to: F T'
+	** Action: derive T on `id` to: F T'
 	Stack: ['$', "E'", "T'", 'F']
-	** Action: reduce F on `id` to: id
+	** Action: derive F on `id` to: id
 	Stack: ['$', "E'", "T'", 'id']
 	** Action: match `id`
 	Stack: ['$', "E'", "T'"]
-	** Action: reduce T' on `+` to: ε
+	** Action: derive T' on `+` to: ε
 	Stack: ['$', "E'"]
-	** Action: reduce E' on `+` to: + T E'
+	** Action: derive E' on `+` to: + T E'
 	Stack: ['$', "E'", 'T', '+']
 	** Action: match `+`
 	Stack: ['$', "E'", 'T']
-	** Action: reduce T on `id` to: F T'
+	** Action: derive T on `id` to: F T'
 	Stack: ['$', "E'", "T'", 'F']
-	** Action: reduce F on `id` to: id
+	** Action: derive F on `id` to: id
 	Stack: ['$', "E'", "T'", 'id']
 	** Action: match `id`
 	Stack: ['$', "E'", "T'"]
-	** Action: reduce T' on `$` to: ε
+	** Action: derive T' on `$` to: ε
 	Stack: ['$', "E'"]
-	** Action: reduce E' on `$` to: ε
+	** Action: derive E' on `$` to: ε
 """
